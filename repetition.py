756d3c0ce3d0871bdd7e1ec183adfe3cadde5491
jack-fitterfly <nikunj.prajapati@fitterfly.in>
2020-11-21 19:30:48 +0530

    escaping-chars

--- repetition.py
+++ repetition.py
@@ -13,4 +13,9 @@ print(re.findall(r"n+m+","nikunnmmnj monalisannnmmm"))
 
 # ? for 0 or more accurance
 
-print(re.findall(r"n?iku","ikummm"))
+print(re.findall(r"n?iku","nikummm"))
+
+#present at least twice
+print(re.findall(r"a.*a","bansns"))
+
+#atleast a present twice capital or small [aA].*[aA]
